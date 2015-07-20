0721c09e9740b7c8f90315c71ebd804fa5d5330a
mbigun <mbigun.net@gmail.com>
2015-07-21 01:38:09 +0300

    #22072015/0137: Test commit

--- seocake/Logic/PostContext.cs
+++ seocake/Logic/PostContext.cs
@@ -7,17 +7,17 @@ using System.Web;
 
 namespace seocake.Logic
 {
-    public class PostContext : DbContext
-    {
-        protected override void OnModelCreating(DbModelBuilder modelBuilder)
-        {
-            modelBuilder.Entity<Posts>().ToTable("Posts");
-        }
-        public PostContext()
-            : base("seocakeEntities")
-        {
-        }
+	public class PostContext : DbContext
+	{
+		protected override void OnModelCreating(DbModelBuilder modelBuilder)
+		{
+			modelBuilder.Entity<Posts>().ToTable("Posts");
+		}
+		public PostContext()
+			: base("seocakeEntities")
+		{
+		}
 
-        public DbSet<Posts> PostSet { get; set; }
-    }
+		public DbSet<Posts> PostSet { get; set; }
+	}
 }
